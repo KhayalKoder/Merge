972ae76360b1e2d8eace5851a06bb290d8d87043
KhayalKoder <pinu40855@gmail.com>
2026-08-02 00:42:54 +0400

    Change reward value and improve formatting

--- reward.js
+++ reward.js
@@ -1,16 +1,17 @@
-const reward = 0.010;
+const reward = 0.005;
 
 setTimeout(function(){
 
-document.getElementById("claimBtn").disabled=false;
-document.getElementById("claimBtn").innerHTML="🎁 Mükafatı Al";
+    document.getElementById("claimBtn").disabled = false;
+    document.getElementById("claimBtn").innerHTML = "🎁 Mükafatı Al";
 
-},30000);
+}, 30000);
 
-document.getElementById("claimBtn").onclick=function(){
+document.getElementById("claimBtn").onclick = function(){
 
-localStorage.setItem("rewardClaimed","1");
+    localStorage.setItem("rewardClaimed", "1");
+    localStorage.setItem("rewardAmount", reward.toFixed(3));
 
-window.location.href="index.html";
+    window.location.href = "index.html";
 
 };
